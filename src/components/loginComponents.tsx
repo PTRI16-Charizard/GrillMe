4cc1ff72d131ee21d06470e09acddbdc3f016924
Jun Inton <juninton@gmail.com>
2025-03-15 11:36:12 -0700

    started Oauth implementation

--- src/components/loginComponents.tsx
+++ src/components/loginComponents.tsx
@@ -1,6 +1,7 @@
 // import React from "react"
 // import { useState } from "react";
 // import { useNavigate } from "react-router";
+import { GoogleLogin } from "@react-oauth/google";
  
 // interface LoginState {
 //     isLoggedIn: boolean;
@@ -10,10 +11,22 @@ function Login (){
     // const navigate = useNavigate();
     // const [isLoggedIn, setIsLoggedIn] = useState<LoginState>(isLoggedIn:false);
     return (
-        <h1>Hello!</h1>
+        <div>
+            <h1>Hello!</h1>
+            <GoogleLogin
+                onSuccess={credentialResponse => {
+                console.log(credentialResponse);
+                }}
+                onError={() => {
+                    console.log('Login Failed');
+                }}
+            />
+        </div>
+
     )
     // oauth
     // on login, should redirect user using react router to /options
+
 }
 
 export default Login;
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId="197457373676-6dvfv01ruaqeb2jjagulcq6ese9dv530.apps.googleusercontent.com">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
